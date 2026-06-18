8230b37644d2efc249dd04fe65f7cffc8fa45916
Pablo <pablosarturi1@gmail.com>
2026-06-18 15:56:07 -0300

    melhora o front; uri do mongo no env

--- src/backend/api.py
+++ src/backend/api.py
@@ -14,6 +14,7 @@ from util import get_access_token, search_games
 
 app = Flask(__name__)
 
+# busca a uri do mongo do env, mas se não exisitr, busca pela default
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
 mongo_client = MongoClient(MONGO_URI)
 reviews_collection = mongo_client["game_rewind"]["reviews"]
@@ -42,7 +43,7 @@ def get_cached_token() -> str:
         _access_token = get_access_token()
     return _access_token
 
-
+# pega os jogos
 @app.get("/games")
 def games():
     query = request.args.get("q", "")
